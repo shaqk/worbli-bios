425e977a447e71d86c6ca60a9a7adb43666c8a08
Robert DeWilder <rob@worbli.io>
2018-10-13 15:02:31 -0400

    removed spaces

--- scripts/05-add-producers.sh
+++ scripts/05-add-producers.sh
@@ -15,5 +15,5 @@ worblim  push action eosio addproducer "{\"producer\":\"eosphere\"}" -p eosio
 worblim  push action eosio addproducer "{\"producer\":\"eosrio\"}" -p eosio
 worblim  push action eosio addproducer "{\"producer\":\"eostribe\"}" -p eosio
 worblim  push action eosio addproducer "{\"producer\":\"infinitybloc\"}" -p eosio
-worblim  push action eosio addproducer "{\"producer\":\"worbli \"}" -p eosio
-worblim  push action eosio addproducer "{\"producer\":\"eosmetal \"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"worbli\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"eosmetal\"}" -p eosio
