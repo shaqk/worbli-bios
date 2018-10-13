34f10cd8406f80c93696d1cdaf41d4ae61d1cfcf
Robert DeWilder <rob@worbli.io>
2018-10-13 11:35:48 -0400

    updated paths and executable

--- scripts/05-add-producers.sh
+++ scripts/05-add-producers.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-worbli push action eosio addproducer "{\"producer\":\"beyondbtc\"}" -p eosio
-worbli push action eosio addproducer "{\"producer\":\"bitfishrocks\"}" -p eosio
-worbli push action eosio addproducer "{\"producer\":\"blocksmithio\"}" -p eosio
-worbli push action eosio addproducer "{\"producer\":\"dune\"}" -p eosio
-worbli push action eosio addproducer "{\"producer\":\"eoscafeblock\"}" -p eosio
-worbli push action eosio addproducer "{\"producer\":\"eoscannon\"}" -p eosio
-worbli push action eosio addproducer "{\"producer\":\"eosdac\"}" -p eosio
-worbli push action eosio addproducer "{\"producer\":\"eosiodetroit\"}" -p eosio
-worbli push action eosio addproducer "{\"producer\":\"eosiosg\"}" -p eosio
-worbli push action eosio addproducer "{\"producer\":\"eosnairobi\"}" -p eosio
-worbli push action eosio addproducer "{\"producer\":\"eosnewyorkio\"}" -p eosio
-worbli push action eosio addproducer "{\"producer\":\"eosphere\"}" -p eosio
-worbli push action eosio addproducer "{\"producer\":\"eosrio\"}" -p eosio
-worbli push action eosio addproducer "{\"producer\":\"eostribe\"}" -p eosio
-worbli push action eosio addproducer "{\"producer\":\"infinitybloc\"}" -p eosio
-worbli push action eosio addproducer "{\"producer\":\"bp.worbli\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"beyondbtc\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"bitfishrocks\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"blocksmithio\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"dune\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"eoscafeblock\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"eoscannon\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"eosdac\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"eosiodetroit\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"eosiosg\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"eosnairobi\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"eosnewyorkio\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"eosphere\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"eosrio\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"eostribe\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"infinitybloc\"}" -p eosio
+worblim  push action eosio addproducer "{\"producer\":\"bp.worbli \"}" -p eosio
