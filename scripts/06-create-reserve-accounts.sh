dda63708fd86adbc9ad432747f1b9987643f16b7
Shaq Kheder <shaq.kheder@gmail.com>
2018-10-24 15:41:37 -0400

    added eostitan reserve account

--- scripts/06-create-reserve-accounts.sh
+++ scripts/06-create-reserve-accounts.sh
@@ -7,3 +7,5 @@ worblim system newaccount eosio --transfer eosnationftw EOS5mjZzBi2sp9eWCPvoRAY7
 worblim system newaccount eosio --transfer gravitypool1 EOS6HDHDrDWuWrCyrsLNgch4smwsXNrqM3cz3tz5wvvSsdTepvxHR --stake-net "500.0000 WBI" --stake-cpu "500.0000 WBI" --buy-ram-kbytes 4
 
 worblim system newaccount eosio --transfer chainrift EOS7RQ6fJTDzZTSoL6tmzXwfs9FLZTUPWDe6pNAhqqWHvcAxG7qdN --stake-net "500.0000 WBI" --stake-cpu "500.0000 WBI" --buy-ram-kbytes 4
+
+worblim system newaccount eosio --transfer eostitan EOS7WjjoUg1tPfpkgYuuFFqAHU5r7p57dBT9KKjFsJgXBmSscQ5dw EOS7Pkpm3b9fqzHaQKfEZE8aa4HrdMPLJJoWYh6p4FLFJv2zV7Ffk --stake-net "500.0000 WBI" --stake-cpu "500.0000 WBI" --buy-ram-kbytes 4
