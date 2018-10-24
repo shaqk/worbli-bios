b60cc6dfd08789db36f23f593cb5cdafd0e4b799
Mourad Ilyes Mlik <dzsi2007@gmail.com>
2018-10-24 10:11:38 +0200

    fixing the script to make it executable

--- scripts/06-create-reserve-accounts.sh
+++ scripts/06-create-reserve-accounts.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 worblim system newaccount eosio --transfer eosnairobi EOS5ogxhfDgEe7rMtQ6qabhHGHZwXziztgs7x46AgMKQHWAjBKN1D --stake-net "500.0000 WBI" --stake-cpu "500.0000 WBI" --buy-ram-kbytes 4
 
 worblim system newaccount eosio --transfer eosnationftw EOS5mjZzBi2sp9eWCPvoRAY7Yhoq1GG16vjsurA6WTNFBjrnU8WYm --stake-net "500.0000 WBI" --stake-cpu "500.0000 WBI" --buy-ram-kbytes 4
